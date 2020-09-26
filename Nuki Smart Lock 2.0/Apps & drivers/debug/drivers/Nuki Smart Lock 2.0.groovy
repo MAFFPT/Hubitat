cf0db5612244bab10c8765e01fee6bb342c4ce81
MAFFPT <33674591+MAFFPT@users.noreply.github.com>
2020-09-25 22:58:09 -0300

    Update Nuki Smart Lock 2.0.groovy

--- Nuki Smart Lock 2.0/Apps & drivers/debug/drivers/Nuki Smart Lock 2.0.groovy
+++ Nuki Smart Lock 2.0/Apps & drivers/debug/drivers/Nuki Smart Lock 2.0.groovy
@@ -18,130 +18,8 @@
 
 import groovy.transform.Field
 
-@Field static Map _lockActions = [1: [actionCode:       1,
-                                      actionName:       "unlock",
-                                      eventName:        "unlock",
-                                      transientState:   "unlocking",
-                                      actionInProgress: "unlocking (waiting for Nuki bridge to finish operation)",
-                                      actionSuccess:    "unlock command successfully sent (waiting for Nuki Bridge confirmation)",
-                                      actionFailure:    "unlocking failed"], 
-                                  
-                                  2: [actionCode:       2,
-                                      actionName:       "lock",
-                                      eventName:        "lock",
-                                      transientState:   "locking",
-                                      actionInProgress: "locking (waiting for Nuki bridge to finish operation)",
-                                      actionSuccess:    "lock command successfully sent (waiting for Nuki Bridge confirmation)",
-                                      actionFailure:    "locking failed"],
-                                  
-                                  3: [actionCode:       3,
-                                      actionName:       "unlatch",
-                                      eventName:        "unlatch",
-                                      transientState:   "unlatching",
-                                      actionInProgress: "unlatching (waiting for Nuki bridge to finish operation)",
-                                      actionSuccess:    "unlatch command successfully sent (waiting for Nuki Bridge confirmation)",
-                                      actionFailure:    "unlatching failed"],
-                                  
-                                  4: [actionCode:       4,
-                                      actionName:       "lock 'n' go",
-                                      eventName:        "lock",
-                                      transientState:   "locking",
-                                      actionInProgress: "pausing 20 seconds before locking (lock 'n' go)",
-                                      actionSuccess:    "'lock 'n' go' command successfully sent (waiting for Nuki Bridge confirmation)",
-                                      actionFailure:    "lock 'n' go failed"],
-                                 
-                                  5: [actionCode:       5,
-                                      actionName:       "lock 'n' go with unlatch",
-                                      eventName:        "lock",
-                                      transientState:   "locking",
-                                      actionInProgress: "locking & unlatching (waiting for Nuki bridge to finish operation)",
-                                      actionSuccess:    "locking & unlatching command successfully sent (waiting for Nuki Bridge confirmation)",
-                                      actionFailure:    "locking & unlatching failed"] \
-                                  ]
-@Field static List _lockStates = [[stateId: 0,
-                                   stateName: "uncalibrated",
-                                   progressText: "lock needs calibration"],
-                                  
-                                  [stateId: 1,
-                                   stateName: "locked",
-                                   progressText: "locking successful"],
-                                 
-                                  [stateId: 2,
-                                   stateName: "unlocking",
-                                   progressText: "unlocking in progress"],
-                                 
-                                  [stateId: 3,
-                                   stateName: "unlocked", 
-                                   progressText: "unlocking successful"],
-                                 
-                                  [stateId: 4,
-                                   stateName: "locking", 
-                                   progressText: "locking in progress"],
-                                 
-                                  [stateId: 5,
-                                   stateName: "unlatched",
-                                   progressText: "unlatch successful"],
-                                 
-                                  [stateId: 6,
-                                   stateName: "unlocked lockngo",
-                                   progressText: "unlocking 'n' go successful"],
-                                 
-                                  [stateId: 7,
-                                   stateName: "unlatching",
-                                   progressText: "unlatching in progress"],
-                                 
-                                  [stateId: 253,
-                                   stateName: "booting",
-                                   progressText: "Nuki bridge booting - please wait"],
-                                 
-                                  [stateId: 254,
-                                   stateName: "motor blocked",
-                                   progressText: "motor blocked - please check your lock"],
-                                 
-                                  [stateId: 255,
-                                   stateName: "undefined",
-                                   progressText: "I don't know what to do ..."]
-                                 ]
-
-
-@Field static List _doorSensorStates = [[stateId: 0,
-                                         stateName: "unavailable",
-                                         sendEvent: false,
-                                         eventText: ""],
-                                  
-                                        [stateId: 1,
-                                         stateName: "deactivated",
-                                         sendEvent: false,
-                                         eventText: ""],
-                                 
-                                        [stateId: 2,
-                                         stateName: "door closed",
-                                         sendEvent: true,
-                                         eventText: "closed"],
-                                 
-                                        [stateId: 3,
-                                         stateName: "door opened",
-                                         sendEvent: true,
-                                         eventText: "open"],
-                                 
-                                        [stateId: 4,
-                                         stateName: "door state unknown",
-                                         sendEvent: false,
-                                         eventText: ""],
-                                 
-                                        [stateId: 5,
-                                         stateName: "calibrating",
-                                         sendEvent: false,
-                                         eventText: ""]
-                                       ]
-
-
-//@Field static Map lockActions2 = [0: "NO_ACTION", 1: "UNLOCK", 2: "LOCK", 3: "UNLATCH", 4: "LOCK_N_GO", 5: "LOCK_N_GO_WITH_UNLATCH"]
-//@Field static Map lockDoorStatus = [0: "UNAVAILABLE", 1: "DEACTIVATED", 2: "DOOR_CLOSED", 3: "DOOR_OPENED", 4: "DOOR_STATE_UNKNOWN", 5: "CALIBRATING"]
-//@Field static Map lockButtonActions = [0: "NO_ACTION", 1: "INTELLIGENT", 2: "UNLOCK", 3: "LOCK", 4: "UNLATCH", 5: "LOCK_N_GO", 6: "SHOW_STATUS"]
-
 @Field static _nukiNamespace = "maffpt.nuki"             // All apps and drivers must be at the same namespace
-@Field static _nukiLockDriverVersion = "0.5.0"           // Current version of this driver
+@Field static _nukiLockDriverVersion = "0.5.1"           // Current version of this driver
 
 @Field static Map _lockDeviceModes = [2: "Door mode"]
 
@@ -886,3 +764,129 @@ def appDebugLogging () { return parent.appDebugLogging () }
 def logDebug (message) { if (appDebugLogging ()) log.debug (message) }
 def logInfo  (message) { log.info (message) }
 def logWarn  (message) { log.warn (message) }
+
+
+// Static Globals
+@Field static Map _lockActions = [1: [actionCode:       1,
+                                      actionName:       "unlock",
+                                      eventName:        "unlock",
+                                      transientState:   "unlocking",
+                                      actionInProgress: "unlocking (waiting for Nuki bridge to finish operation)",
+                                      actionSuccess:    "unlock command successfully sent (waiting for Nuki Bridge confirmation)",
+                                      actionFailure:    "unlocking failed"], 
+                                  
+                                  2: [actionCode:       2,
+                                      actionName:       "lock",
+                                      eventName:        "lock",
+                                      transientState:   "locking",
+                                      actionInProgress: "locking (waiting for Nuki bridge to finish operation)",
+                                      actionSuccess:    "lock command successfully sent (waiting for Nuki Bridge confirmation)",
+                                      actionFailure:    "locking failed"],
+                                  
+                                  3: [actionCode:       3,
+                                      actionName:       "unlatch",
+                                      eventName:        "unlatch",
+                                      transientState:   "unlatching",
+                                      actionInProgress: "unlatching (waiting for Nuki bridge to finish operation)",
+                                      actionSuccess:    "unlatch command successfully sent (waiting for Nuki Bridge confirmation)",
+                                      actionFailure:    "unlatching failed"],
+                                  
+                                  4: [actionCode:       4,
+                                      actionName:       "lock 'n' go",
+                                      eventName:        "lock",
+                                      transientState:   "locking",
+                                      actionInProgress: "pausing 20 seconds before locking (lock 'n' go)",
+                                      actionSuccess:    "'lock 'n' go' command successfully sent (waiting for Nuki Bridge confirmation)",
+                                      actionFailure:    "lock 'n' go failed"],
+                                 
+                                  5: [actionCode:       5,
+                                      actionName:       "lock 'n' go with unlatch",
+                                      eventName:        "lock",
+                                      transientState:   "locking",
+                                      actionInProgress: "locking & unlatching (waiting for Nuki bridge to finish operation)",
+                                      actionSuccess:    "locking & unlatching command successfully sent (waiting for Nuki Bridge confirmation)",
+                                      actionFailure:    "locking & unlatching failed"] \
+                                  ]
+@Field static List _lockStates = [[stateId: 0,
+                                   stateName: "uncalibrated",
+                                   progressText: "lock needs calibration"],
+                                  
+                                  [stateId: 1,
+                                   stateName: "locked",
+                                   progressText: "locking successful"],
+                                 
+                                  [stateId: 2,
+                                   stateName: "unlocking",
+                                   progressText: "unlocking in progress"],
+                                 
+                                  [stateId: 3,
+                                   stateName: "unlocked", 
+                                   progressText: "unlocking successful"],
+                                 
+                                  [stateId: 4,
+                                   stateName: "locking", 
+                                   progressText: "locking in progress"],
+                                 
+                                  [stateId: 5,
+                                   stateName: "unlatched",
+                                   progressText: "unlatch successful"],
+                                 
+                                  [stateId: 6,
+                                   stateName: "unlocked lockngo",
+                                   progressText: "unlocking 'n' go successful"],
+                                 
+                                  [stateId: 7,
+                                   stateName: "unlatching",
+                                   progressText: "unlatching in progress"],
+                                 
+                                  [stateId: 253,
+                                   stateName: "booting",
+                                   progressText: "Nuki bridge booting - please wait"],
+                                 
+                                  [stateId: 254,
+                                   stateName: "motor blocked",
+                                   progressText: "motor blocked - please check your lock"],
+                                 
+                                  [stateId: 255,
+                                   stateName: "undefined",
+                                   progressText: "I don't know what to do ..."]
+                                 ]
+
+
+@Field static List _doorSensorStates = [[stateId: 0,
+                                         stateName: "unavailable",
+                                         sendEvent: false,
+                                         eventText: ""],
+                                  
+                                        [stateId: 1,
+                                         stateName: "deactivated",
+                                         sendEvent: false,
+                                         eventText: ""],
+                                 
+                                        [stateId: 2,
+                                         stateName: "door closed",
+                                         sendEvent: true,
+                                         eventText: "closed"],
+                                 
+                                        [stateId: 3,
+                                         stateName: "door opened",
+                                         sendEvent: true,
+                                         eventText: "open"],
+                                 
+                                        [stateId: 4,
+                                         stateName: "door state unknown",
+                                         sendEvent: false,
+                                         eventText: ""],
+                                 
+                                        [stateId: 5,
+                                         stateName: "calibrating",
+                                         sendEvent: false,
+                                         eventText: ""]
+                                       ]
+
+
+//@Field static Map lockActions2 = [0: "NO_ACTION", 1: "UNLOCK", 2: "LOCK", 3: "UNLATCH", 4: "LOCK_N_GO", 5: "LOCK_N_GO_WITH_UNLATCH"]
+//@Field static Map lockDoorStatus = [0: "UNAVAILABLE", 1: "DEACTIVATED", 2: "DOOR_CLOSED", 3: "DOOR_OPENED", 4: "DOOR_STATE_UNKNOWN", 5: "CALIBRATING"]
+//@Field static Map lockButtonActions = [0: "NO_ACTION", 1: "INTELLIGENT", 2: "UNLOCK", 3: "LOCK", 4: "UNLATCH", 5: "LOCK_N_GO", 6: "SHOW_STATUS"]
+
+
