664f91022f79d983d2748a64cf9d92a99058c8f1
MAFFPT <33674591+MAFFPT@users.noreply.github.com>
2022-08-22 21:20:02 +0100

    Update Nuki Smart Lock 2.0 Integration.groovy

--- Nuki Smart Lock 2.0/Apps & drivers/debug/apps/Nuki Smart Lock 2.0 Integration.groovy
+++ Nuki Smart Lock 2.0/Apps & drivers/debug/apps/Nuki Smart Lock 2.0 Integration.groovy
@@ -20,7 +20,7 @@ import groovy.transform.Field
 
 @Field static _nukiNamespace = "maffpt.nuki"
 @Field static _nukiDriverNameBridge = "Nuki Bridge"   	             // name of the device type = driver name
-@Field static _nukiDriverNameLock = "Nuki Smart Lock 2.0"            // Nuki Smart Lock 2.0/3.0's device driver name
+@Field static _nukiDriverNameLock = "Nuki Smart Lock 2.0/3.0"        // Nuki Smart Lock 2.0/3.0's device driver name
 @Field static _nukiDriverNameOpener = "Nuki Opener"                  // Nuki Opener's device driver name
 @Field static _nukiAppName = "Nuki Smart Lock 2.0 Integration"       // This app
 
